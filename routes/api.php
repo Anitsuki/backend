992fd5c82570752c6225974407a32793d4e118d8
alix1383 <dev.alix13@gmail.com>
2025-01-30 11:58:41 +0330

    Add API route for fetching anime schedule with caching and create live_chart_cookie.json

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,23 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::prefix('v1/anime')->group(function () {
+    Route::get('/schedule', function () {
+
+        if (Cache::has('anime_schedule')) {
+            return Cache::get('anime_schedule');
+        }
+
+
+        Artisan::call('app:schedule');
+        return Cache::get('anime_schedule');
+    });
 });
